77d117732a1d4be53eca4f909603c4bb2bc4f5e8
Chad Curtis <chad@chadwick.site>
2025-06-09 17:06:29 +0000

    Get rid of the same odd banner in profiles as well.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -165,9 +165,6 @@ export default function Profile() {
         opacity: 0.25
       }}></div>
       
-      {/* Full-width top depth shadow with better fade - adventure mode only */}
-      <div className="absolute top-0 left-0 right-0 h-12 adventure:bg-gradient-to-b adventure:from-black/8 adventure:via-black/4 adventure:via-black/2 adventure:via-black/1 adventure:to-transparent pointer-events-none z-20"></div>
-      
       <DesktopHeader />
 
       <div className="container mx-auto px-4 py-8">
